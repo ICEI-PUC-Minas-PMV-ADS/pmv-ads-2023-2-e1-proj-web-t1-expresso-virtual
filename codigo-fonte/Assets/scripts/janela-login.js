75b418be31d4cd955ed54a5ffef864c91cd7e4d9
vitor-sld <vitor.cteles@gmail.com>
2023-11-23 12:12:29 -0300

    Modularizando função Login

--- codigo-fonte/Assets/scripts/janela-login.js
+++ codigo-fonte/Assets/scripts/janela-login.js
@@ -1,3 +1,7 @@
+
+import { cadastro, verificarLogin } from './crudLogin.js';
+
+
 window.addEventListener('load',() => {
 
     groupNome.style.display = "none"
@@ -76,58 +80,4 @@ window.addEventListener('load',() => {
             return false
         }
     }
-
-
-    function geradorId() {
-        try {
-            let ultimoIDCadastrado = localStorage.getItem("ultimoIDCadastrado")
-            let idConvertido = JSON.parse(ultimoIDCadastrado);
-            let idAtual = idConvertido + 1;
-            localStorage.setItem('ultimoIDCadastrado', JSON.stringify(idAtual));
-            return idAtual;
-        } catch (error) {
-            console.log(error)
-            let idAtual = 1;
-            localStorage.setItem('ultimoIDCadastrado', JSON.stringify(idAtual));
-            return idAtual;
-        }
-    }
-
-    async function cadastro(nomeUsuario, emailUsuario, senhaUsuario) {
-        try {
-            let usuariosCadastrados = localStorage.getItem('usuarios');
-            let ArrUsuariosCadastro = JSON.parse(usuariosCadastrados);
-            idUsuario = await geradorId();
-            let cadastro = { nome: nomeUsuario, email: emailUsuario, id: idUsuario, senha: senhaUsuario };
-            ArrUsuariosCadastro.push(cadastro);
-            localStorage.setItem("usuarios", JSON.stringify(ArrUsuariosCadastro));
-        } catch (error) {
-            let arrInicial = [{ nome: nomeUsuario, email: emailUsuario, id: idUsuario, senha: senhaUsuario }]
-            localStorage.setItem('usuarios', JSON.stringify(arrInicial))
-        }
-    }
-    function verificarLogin(emailUsuario, senhaUsuario) {
-
-        let usuariosCadastrados = localStorage.getItem('usuarios');
-        let ArrUsuariosCadastro = JSON.parse(usuariosCadastrados);
-
-        let verificarLoginUsuario = ArrUsuariosCadastro.find((t) => {
-            if (t.email == emailUsuario && t.senha == senhaUsuario) {
-                let usuarioLogado = { nome: t.nome, email: t.email, id: t.id, senha: t.senha }
-                return usuarioLogado
-            }
-        })
-        if (verificarLoginUsuario != null && verificarLoginUsuario != undefined) {
-            iniciarSessao(verificarLoginUsuario)
-        } else {
-            alert("usuário não cadastrado")
-            console.log(verificarLoginUsuario)
-        }
-
-    }
-
-    function iniciarSessao(usuarioLogado) {
-        console.log(usuarioLogado)
-    }
-
 })
