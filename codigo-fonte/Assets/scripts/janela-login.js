0143bf73a9c3b06d6b3aa4b2c3a915ebae6818ed
vitor-sld <vitor.cteles@gmail.com>
2023-11-23 10:42:18 -0300

    adicionando login com Local Storage

--- codigo-fonte/Assets/scripts/janela-login.js
+++ codigo-fonte/Assets/scripts/janela-login.js
@@ -1,15 +1,125 @@
-let formularioSection = document.getElementById('formularioSectionLogin')
-let botaoLinkLogin = document.getElementById('btn-link-login').addEventListener('click', (e)=>{
-    e.preventDefault();
-    formularioSection.classList.toggle("formulario--is-hidden")
-});
-let btnCloseForm = document.getElementById('btn-close-form').addEventListener('click', (e)=>{
-    e.preventDefault();
-    formularioSection.classList.toggle("formulario--is-hidden")
-});
+    //some ao clicarmos em login
+    groupNome.style.display = "none"
+    btnCadastro.style.display = "none";
+    linkLogin.style.display = "none";
+    //aparece ao clicar em Login
+    linkCadastro.style.display = "block";
+    btnLogin.style.display = "block"
 
 
-let botaoLogin = document.getElementById('btn-login').addEventListener('click', (e)=>{
-    e.preventDefault();
-    alert('Hello World')
-});
+closeModal.onclick = () => {
+    login.style.display = "none";
+}
+
+btnModalLogin.onclick = () => {
+    login.style.display = "flex";
+
+    //some ao clicarmos em login
+    groupNome.style.display = "none"
+    btnCadastro.style.display = "none";
+    linkLogin.style.display = "none";
+    //aparece ao clicar em Login
+    linkCadastro.style.display = "block";
+    btnLogin.style.display = "block"
+}
+
+linkLogin.onclick = () => {
+    //some ao clicarmos em login
+    groupNome.style.display = "none"
+    btnCadastro.style.display = "none";
+    linkLogin.style.display = "none";
+    //aparece ao clicar em Login
+    linkCadastro.style.display = "block";
+    btnLogin.style.display = "block"
+
+}
+
+linkCadastro.onclick = () => {
+    //some ao clicarmos em login
+    linkCadastro.style.display = "none";
+    btnLogin.style.display = "none";
+    //aparece ao clicar em Login
+    groupNome.style.display = "flex";
+    linkLogin.style.display = "block";
+    btnCadastro.style.display = "block";
+
+}
+let nomeUsuario, emailUsuario, senhaUsuario;
+inputNome.onkeyup = () => { nomeUsuario = inputNome.value; }
+inputEmail.onkeyup = () => { emailUsuario = inputEmail.value; }
+inputSenha.onkeyup = () => { senhaUsuario = inputSenha.value; }
+
+
+
+btnCadastro.onclick = () => {
+    let validadorCampos = verificador()
+
+    if (validadorCampos) {
+        cadastro(nomeUsuario, emailUsuario, senhaUsuario)
+    } else {
+        alert(inputNome.value.length)
+    }
+}
+
+btnLogin.onclick = () => {
+    verificarLogin(emailUsuario, senhaUsuario)
+}
+
+function verificador() {
+    if (
+        inputNome.value.length > 3 &&
+        inputEmail.value.length > 3 &&
+        inputSenha.value.length > 3
+    ) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+function geradorId() {
+    try {
+        let ultimoIDCadastrado = localStorage.getItem("ultimoIDCadastrado")
+        let idConvertido = JSON.parse(ultimoIDCadastrado);
+        let idAtual = idConvertido + 1;
+        localStorage.setItem('ultimoIDCadastrado', JSON.stringify(idAtual));
+        return idAtual;
+    } catch (error) {
+        console.log(error)
+        let idAtual = 1;
+        localStorage.setItem('ultimoIDCadastrado', JSON.stringify(idAtual));
+        return idAtual;
+    }
+}
+
+async function cadastro(nomeUsuario, emailUsuario, senhaUsuario) {
+    try {
+        let usuariosCadastrados = localStorage.getItem('usuarios');
+        let ArrUsuariosCadastro = JSON.parse(usuariosCadastrados);
+        idUsuario = await geradorId();
+        let cadastro = { nome: nomeUsuario, email: emailUsuario, id: idUsuario, senha: senhaUsuario };
+        ArrUsuariosCadastro.push(cadastro);
+        localStorage.setItem("usuarios", JSON.stringify(ArrUsuariosCadastro));
+    } catch (error) {
+        let arrInicial = [{ nome: nomeUsuario, email: emailUsuario, id: idUsuario, senha: senhaUsuario }]
+        localStorage.setItem('usuarios', JSON.stringify(arrInicial))
+    }
+}
+function verificarLogin(emailUsuario, senhaUsuario) {
+    try {
+        let usuariosCadastrados = localStorage.getItem('usuarios');
+        let ArrUsuariosCadastro = JSON.parse(usuariosCadastrados);
+
+        let verificarLoginUsuario = ArrUsuariosCadastro.find((t) => {
+            if (t.email == emailUsuario && t.senha == senhaUsuario){
+                return true
+            }
+        })
+
+        alert (verificarLoginUsuario)
+    }
+    catch {
+
+}
+}
