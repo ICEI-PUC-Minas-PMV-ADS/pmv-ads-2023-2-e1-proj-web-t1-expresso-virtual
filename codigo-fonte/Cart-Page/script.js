1739311b7d37e87625a4c9ba0e9cc98d2f838530
Laura Lanna Brandão <laura.lannab@gmail.com>
2023-11-24 22:23:25 -0300

    Carrinho funcional, com documentacao do js e correcao da navegacao para o menu

---
+++ codigo-fonte/Cart-Page/script.js
@@ -0,0 +1,166 @@
+//Armazena os dados dos produtos obtidos do localStorage com a chave "product".
+let productsData = JSON.parse(localStorage.getItem("product"));
+//Armazena os dados do carrinho obtidos do localStorage com a chave "cart".
+let cartData = JSON.parse(localStorage.getItem("cart"));
+//Armazena o valor total da compra.
+let total = 0;
+
+//Função que recebe um valor numérico e retorna uma string formatada como
+//moeda brasileira (formata o valor vindo como 0.00 para R$0,00).
+function formatPrice(price) {
+  return price.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
+
+//função responsável por renderizar dinamicamente o conteúdo do carrinho na página.
+//Itera sobre groupedCartData, que é um array contendo objetos agrupados pelos IDs dos produtos e suas quantidades.
+//Cria elementos HTML dinamicamente para cada item do carrinho.
+//Calcula o total da compra usando reduce.
+//Atualiza o total na página usando textContent.
+function renderCart() {
+  const cartContainer = document.getElementById("cart-container");
+  const totalElement = document.getElementById("preco-total");
+
+  // Limpar o conteúdo atual do carrinho
+  cartContainer.innerHTML = "";
+
+  // Verificar se o carrinho não possui nada (cartData.length === 0) ou se retornar null,
+  //significa que ele está vazio e irá retornar uma mensagem de texto e ajustar o valor total para 0
+  if (!cartData || cartData.length === 0) {
+    cartContainer.innerHTML = "<p>O carrinho está vazio :(</p>";
+    totalElement.textContent = "R$ 0,00";
+    return;
+  }
+
+  const contagemDeIds = {};
+  const groupedCartData = [];
+
+  // Agrupar itens do carrinho pelo ID e contar a quantidade
+  cartData.forEach((item) => {
+    const { id } = item;
+    contagemDeIds[id] = (contagemDeIds[id] || 0) + 1;
+
+    // Verificar se o item já foi agrupado
+    const existingItem = groupedCartData.find(
+      (groupedItem) => groupedItem.id === id
+    );
+
+    if (!existingItem) {
+      // Se o item ainda não foi agrupado, adicionar ao grupo sem a quantidade
+      groupedCartData.push({ ...item, quantity: 0 });
+    }
+  });
+
+  // Atualizar a quantidade no grupo
+  groupedCartData.forEach((groupedItem) => {
+    groupedItem.quantity = contagemDeIds[groupedItem.id];
+  });
+
+  // Adicionar conteúdo do carrinho dinamicamente
+  groupedCartData.forEach((item) => {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart");
+
+    // Adicionar o conteúdo do carrinho ao item do carrinho HTML
+    cartItem.innerHTML = `
+        <img class="cart-img" src=${item.image} />
+        <div class="cart-container">
+          <div class="cart-info">
+            <p>Produto</p>
+            <p>${item.name}</p>
+          </div>
+          <div class="cart-info">
+            <p>Quantidade</p>
+            <div class="cart-info-button">
+              <img class="cart-info-button-img" src="./img/seta-para-cima.png" onclick="increaseQuantity(${
+                item.id
+              })" />
+              <p>${item.quantity}</p>
+              <img class="cart-info-button-img" src="./img/seta-para-baixo.png" onclick="decreaseQuantity(${
+                item.id
+              })" />
+            </div>
+          </div>
+          <div class="cart-info">
+            <p>Valor</p>
+            <p>${formatPrice(item.price)}</p>
+          </div>
+        </div>
+      `;
+
+    // Adicionar o item do carrinho ao carrinho HTML
+    cartContainer.appendChild(cartItem);
+  });
+
+  // Calcular o total
+  const total = groupedCartData.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  // Atualizar o total na página
+  totalElement.textContent = `${formatPrice(total)}`;
+}
+
+//aumenta a quantidade de um produto no carrinho ao clicar na seta para cima.
+//Encontra o item no carrinho com base no ID e o adiciona novamente ao carrinho.
+//Chama renderCart para atualizar a exibição do carrinho.
+function increaseQuantity(productId) {
+  // Encontrar o item no carrinho pelo ID
+  const cartItem = cartData.find((item) => item.id === productId);
+
+  // Adicionar o item ao carrinho
+  if (cartItem) {
+    cartData.push({ ...cartItem });
+  }
+
+  // Renderizar o carrinho novamente
+  renderCart();
+}
+
+//diminui a quantidade de um produto no carrinho ao clicar na seta para baixo.
+//Encontra o índice do item no carrinho com base no ID.
+//Se a quantidade for maior que 1, diminui a quantidade; caso contrário, remove o produto do carrinho.
+//Chama renderCart para atualizar a exibição do carrinho.
+function decreaseQuantity(productId) {
+  // Encontrar o índice do item no carrinho pelo ID
+  const index = cartData.findIndex((item) => item.id === productId);
+
+  // Se o produto existir no carrinho e a quantidade for maior que 1, diminua a quantidade
+  if (index !== -1 && cartData[index].quantity > 1) {
+    cartData[index].quantity--;
+
+    // Renderizar o carrinho novamente
+    renderCart();
+  } else if (index !== -1) {
+    // Se a quantidade for 1 ou menos, remova o produto do carrinho
+    cartData.splice(index, 1);
+
+    // Renderizar o carrinho novamente
+    renderCart();
+  }
+}
+
+// é chamada ao finalizar a compra.
+//Remove o carrinho do localStorage, redefine cartData como um array vazio e chama renderCart.
+//Exibe um alerta informando que a compra foi finalizada e o carrinho foi esvaziado.
+function finalizePurchase() {
+  // Verifica se possui itens no carrinho, e exibe um alerta
+  if (!cartData || cartData.length === 0) {
+    alert("Seu carrinho está vazio! Compra não efetuada!");
+    return;
+  }
+
+  // Limpar o carrinho no localStorage
+  localStorage.removeItem("cart");
+
+  // Atualizar o carrinho local
+  cartData = [];
+
+  // Renderizar o carrinho novamente
+  renderCart();
+
+  alert("Compra finalizada! O carrinho foi esvaziado.");
+}
+
+// Chame a função renderCart() para inicializar o carrinho na página
+renderCart();
--- codigo-fonte/Menu-page/products.js
+++ codigo-fonte/Menu-page/products.js
@@ -1,12 +1,19 @@
+//// Dados dos produtos disponíveis na cafeteria
 let cafeteriaProducts = {
   cafeA: {
+    // Identificador único do produto
     id: 1,
+    // Nome do produto
     name: "Café Expresso",
+    // Descrição do produto
     description:
       "O café expresso é uma bebida preparada pela passagem de água quente pressurizada através de café moído finamente. É conhecido por sua intensidade e sabor encorpado. Desfrute do aroma robusto e do sabor rico do nosso café expresso feito com grãos de alta qualidade.",
+    //Preço do produto
     price: 2.99,
+    // Texto alternativo para a imagem
     alt: "Café expresso",
-    image: "img/cafe-expresso.jpg"
+    // Caminho da imagem do produto
+    image: "img/cafe-expresso.jpg",
   },
   cafeB: {
     id: 2,
@@ -15,7 +22,7 @@ let cafeteriaProducts = {
       "Delicie-se com nosso croissant recém-assado, uma deliciosa massa folhada em camadas, crocante por fora e macia por dentro. Este clássico francês é perfeito para o café da manhã ou um lanche a qualquer hora do dia. Desfrute da combinação irresistível de textura e sabor.",
     price: 5.99,
     alt: "Croissant",
-    image: "img/croissant.jpg"
+    image: "img/croissant.jpg",
   },
   cafeC: {
     id: 3,
@@ -24,7 +31,7 @@ let cafeteriaProducts = {
       "Experimente a autêntica receita mineira em nosso pão de queijo. Feito com queijo de alta qualidade e uma massa leve e aerada, cada mordida é uma explosão de sabor. Perfeito para qualquer momento do dia, nosso pão de queijo é uma escolha irresistível para os amantes de queijo.",
     price: 3.99,
     alt: "Pão de Queijo",
-    image: "img/pao-de-queijo.jpg"
+    image: "img/pao-de-queijo.jpg",
   },
   cafeD: {
     id: 4,
@@ -33,7 +40,7 @@ let cafeteriaProducts = {
       "Refresque-se com nosso delicioso frappé. Uma mistura perfeita de café gelado, leite, gelo e um toque de sabor. Desfrute da textura cremosa e da combinação única de café gelado que torna nosso frappé uma escolha refrescante para qualquer dia. Uma verdadeira indulgência para os amantes de café.",
     price: 8.99,
     alt: "Frapé",
-    image: "img/frape.jpg"
+    image: "img/frape.jpg",
   },
   cafeE: {
     id: 5,
@@ -42,21 +49,27 @@ let cafeteriaProducts = {
       "Desfrute do sabor caseiro do nosso bolo de fubá com queijo. Com uma textura macia e uma combinação perfeita de fubá e queijo, este bolo é uma verdadeira delícia. Ideal para o café da tarde ou como sobremesa, nosso bolo de fubá com queijo é uma opção reconfortante e saborosa.",
     price: 2.79,
     alt: "Bolo de Fubá com Queijo",
-    image: "img/bolo.jpg"
+    image: "img/bolo.jpg",
   },
 };
 
+// Verifica se há dados de produtos no localStorage, se não houver, adiciona os dados padrão
 let productsList = JSON.parse(localStorage.getItem("product"));
 if (productsList == null) {
   localStorage.setItem("product", JSON.stringify(cafeteriaProducts));
 }
 
+// Recupera a lista de produtos do localStorage
 let list = JSON.parse(localStorage.getItem("product"));
 
+// Variável para armazenar a representação HTML dos produtos
 let products = "";
+
+// Itera sobre a lista de produtos para criar a representação HTML e renderizar de forma dinâmica
 for (let key in list) {
   if (list.hasOwnProperty(key)) {
     const product = list[key];
+    // Constrói a representação HTML para cada produto
     products += `<div class="cafe2">
       <img class="img-cafe" width="500px" src=${product.image} alt=${product.alt}>
       <div class="cafeA">
@@ -72,16 +85,23 @@ for (let key in list) {
   }
 }
 
+// Adiciona a representação HTML dos produtos ao elemento com o ID "products-list"
 document.getElementById("products-list").innerHTML = products;
 
+// Função para adicionar um produto ao carrinho
 function addToCart(productId) {
+  // Recupera o produto selecionado com base no ID
   const product = cafeteriaProducts[productId];
 
+  // Recupera o carrinho do localStorage ou cria um novo se não existir
   let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+  // Adiciona o produto ao carrinho
   cart.push(product);
 
+  // Atualiza o carrinho no localStorage
   localStorage.setItem("cart", JSON.stringify(cart));
 
+  // Exibe um alerta informando que o produto foi adicionado ao carrinho
   alert("Produto adicionado ao carrinho!");
 }
