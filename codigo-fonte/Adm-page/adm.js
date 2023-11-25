33e7ce484819f1ef5b3fb628a26809af74636f14
Lucas Gonçalves <144072871+Souzaa86@users.noreply.github.com>
2023-11-25 17:40:15 -0300

    JavaScript da pag ADM

---
+++ codigo-fonte/Adm-page/adm.js
@@ -0,0 +1,47 @@
+let usuariosCadastrados = localStorage.getItem('usuarios');
+let ArrUsuariosCadastro = JSON.parse(usuariosCadastrados);
+
+
+
+// console.log(dadosArmazenados); é pra exibir os dados no console ou nao?
+
+puxar.onclick = function () {
+    let dadosArmazenados = JSON.parse(localStorage.getItem('usuarios'));
+
+
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    let tabela = document.getElementById('tabela');
+
+
+    function adicionarLinha(nome, email, id) {
+        let novaLinha = tabela.insertRow();
+        let colunaNome = novaLinha.insertCell(0);
+        let colunaEmail = novaLinha.insertCell(1);
+        let colunaId = novaLinha.insertCell(2);
+
+        colunaNome.innerHTML = nome;
+        colunaEmail.innerHTML = email;
+        colunaId.innerHTML = id;
+    }
+
+
+    document.getElementById('puxar').addEventListener('click', function () {
+
+        let dadosArmazenados = JSON.parse(localStorage.getItem('usuarios'));
+
+
+        tabela.innerHTML = "<tr><th>Nome</th><th>Email</th><th>ID</th></tr>";
+
+
+        if (dadosArmazenados) {
+            dadosArmazenados.forEach(function (cliente) {
+                adicionarLinha(cliente.nome, cliente.email, cliente.id);
+            });
+        }
+    });
+});
+
+
